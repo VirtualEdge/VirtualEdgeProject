ba9ad438fcab6906baacf4adbf61f843ffc1d676
Deuvient <martin.mccaslin@go.edgehill.ac.uk>
2015-03-27 16:23:58 +0000

    My part of the application so far
    lolololololol

---
+++ andriods/MyApplication3/app/src/main/java/com/example/martin/myapplication/Menu2_Fragment.java
@@ -0,0 +1,26 @@
+package com.example.martin.myapplication;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Martin on 03/03/2015.
+ */
+public class Menu2_Fragment extends android.support.v4.app.Fragment {
+    View rootview;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        rootview = inflater.inflate(R.layout.map2page_alt_layout, container, false);
+        return rootview;
+    }
+    public void clickOne(){
+
+
+
+    }
+}
